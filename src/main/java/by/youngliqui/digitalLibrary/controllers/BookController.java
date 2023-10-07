dd3d210b894c4bc5ae3ed1ff8dc83ab46e36952c
youngliqui <super.pav-79@yandex.by>
2023-10-07 11:12:56 +0300

    creating book

--- src/main/java/by/youngliqui/digitalLibrary/controllers/BookController.java
+++ src/main/java/by/youngliqui/digitalLibrary/controllers/BookController.java
@@ -1,4 +1,40 @@
 package by.youngliqui.digitalLibrary.controllers;
 
+import by.youngliqui.digitalLibrary.dao.BookDAO;
+import by.youngliqui.digitalLibrary.models.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/books")
 public class BookController {
+
+    private final BookDAO bookDAO;
+
+    @Autowired
+    public BookController(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model){
+        model.addAttribute(bookDAO.index());
+        return "books/index";
+    }
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") Book book) {
+        bookDAO.save(book);
+        return "redirect:/books";
+    }
 }
